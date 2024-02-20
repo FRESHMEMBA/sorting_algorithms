43411c31a007f76edc58397dbd29edd5a83f6998
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-02-20 14:34:57 +0200

    0. Bubble sort

--- sort.h
+++ sort.h
@@ -20,5 +20,7 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void swap(int *var1, int *var2);
+void bubble_sort(int *array, size_t size);
 
 #endif /*SORT_H*/
