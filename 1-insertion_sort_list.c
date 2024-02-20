c3920fd7b9cad4bb9b6ad49cc493297802aefd67
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-02-20 11:48:46 -0800

    1. Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,39 +1,5 @@
 #include "sort.h"
 
-/**
- * sorted_insert - inserts a new node in a sorted list.
- * @list: The list in which a new node is to be inserted.
- * @new_node: The new node to be inserted in the list.
- * Return: None
- */
-void sorted_insert(listint_t **list, listint_t *new_node)
-{
-    listint_t *current;
-
-    if (*list == NULL || (*list)->n >= new_node->n)
-    {
-        new_node->next = *list;
-        new_node->prev = NULL;
-        if (*list != NULL)
-            (*list)->prev = new_node;
-        *list = new_node;
-    }
-    else
-    {
-        current = *list;
-        while (current->next != NULL && current->next->n < new_node->n)
-            current = current->next;
-
-        new_node->next = current->next;
-        if (current->next != NULL)
-            current->next->prev = new_node;
-        current->next = new_node;
-        new_node->prev = current;
-    }
-
-    print_list(*list);
-}
-
 /** 
  * insertion_sort_list - Sorts  doubly linked list of integers in ascending
  * order using the inserton sort algorithm.
@@ -72,7 +38,7 @@ void insertion_sort_list(listint_t **list)
         }
 
         current = next;
-        print_list(current);
+        print_list(sorted);
     }
 
     *list = sorted;
