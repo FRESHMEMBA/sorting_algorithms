6eebd277c3d8d13082f7b4c8e7fda3630c994771
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-02-20 05:15:05 -0800

    0. Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,16 +1,4 @@
 #include "sort.h"
-/**
- * swap - swaps the values of two variables.
- * @var1: The variable whose value is to be swapped (with var2).
- * @var2: The variable whose value is to be swapped (with var1).
- * Return: None
- */
-void swap(int *var1, int *var2)
-{
-    int temp = *var1;
-    *var1 = *var2;
-    *var2 = temp;
-}
 
 /**
  * bubble_sort - sorts an array of integers in ascending order using the
@@ -21,13 +9,15 @@ void swap(int *var1, int *var2)
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
+    size_t i, j, temp;
 
     for (i = 0; i < size - 1; i++)
         for (j = 0; j < size - i - 1; j++)
             if (array[j] > array[j + 1])
 	    {
-                swap(&array[j], &array[j + 1]);
-                print_array(array, size);
+		    temp = array[j];
+		    array[j] = array[j + 1];
+		    array[j + 1] = temp;
+		    print_array(array, size);
 	    }
 }
