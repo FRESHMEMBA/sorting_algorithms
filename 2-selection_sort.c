d09decea60d7ef7f72675c003ea3f8bcd5dea284
Sibonelo Mazibuko <123705498+FRESHMEMBA@users.noreply.github.com>
2024-02-22 17:08:41 +0200

    2. Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,10 +14,10 @@ void selection_sort(int *array, size_t size)
     size_t i, j;
     int min, temp;
 
-    for (i = 0; i < size - 1; i++)
+    for (i = 0; i < size; i++)
     {
         min = i;
-        for (j = i + 1; j < size - 1; j++)
+        for (j = i + 1; j < size; j++)
         {
             if (array[j] < array[min])
                 min = j;
